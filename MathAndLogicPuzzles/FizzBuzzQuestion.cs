a14a11645318d77f12cdf9fd3a785e25b546a3ce
Sahar Dastitash <sahar.dastitash@gmail.com>
2022-06-20 14:13:31 -0700

    Add project files.

---
+++ MathAndLogicPuzzles/FizzBuzzQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MathAndLogicPuzzles
+{
+    public class FizzBuzzQuestion
+    {
+        public static void FizzBuzz(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                // we use the mod operator
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ MathAndLogicPuzzles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MathAndLogicPuzzles
+{
+    // FizzBuzz: Output numbers from 1 to x. If the number is divisible
+    // by 3, replace it with "Fizz". If it is divisible by 5, replace
+    // it with "Buzz". If it is divisible by 3 and 5 replace it with
+    // "FizzBuzz".
+    // eg:
+    // 1
+    // 2
+    // Fizz
+    // 4
+    // Buzz
+    // 6
+    // -
+    // 14
+    // FizzBuzz
+    // 16
+    // -
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            FizzBuzzQuestion.FizzBuzz(16);
+            Console.ReadLine();
+        }
+
+
+    }
+}
